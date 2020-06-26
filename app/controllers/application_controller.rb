5e8e726faeab8accfab151614c55038d08788b3a
Hirano-Tech <Information@Hirano.cfbx.jp>
2020-06-26 17:05:16 +0900

    Implement a form for submission.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,17 @@
 class ApplicationController < ActionController::Base
   def index
     @posts = Post.all
+
+    @post = Post.new
+  end
+
+  def create
+    Post.create(post_params)
+    redirect_to root_path
+  end
+
+  private
+  def post_params
+    params.require(:post).permit(:title, :content)
   end
 end
