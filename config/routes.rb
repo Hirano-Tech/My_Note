5706429ec474ecae079f4bba3a8b36c39faa7314
Hirano-Tech <Information@Hirano.cfbx.jp>
2020-06-29 14:23:31 +0900

    Install Devise.

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
+  devise_for :users
   root to: 'application#index'
     # ルートパス （get '/', to: '[コントローラー名]#[アクション名]'） にリクエストする。
 
-    resources :application, only: [:index]
+  resources :application, only: [:create, :destroy]
 end
